af1a35e9db54e079dadacf8e1b9c21db1f637efe
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2017-12-14 17:22:22 -0300

    Create a basic test environmet with a simple example

---
+++ app/sorters/quick-sort.js
@@ -0,0 +1,26 @@
+export default function quickSort(arr) {
+  console.log("\narr: ", arr);
+
+  if (arr.length <= 1) return arr;
+  const a = [];
+  const z = [];
+  const l = arr.length - 1;
+  const p = arr[l];
+
+  for (let i = 0; i < l; i++){
+    const e = arr[i];
+
+    if (e <= p) {
+      a.push(e);
+    } else {
+      z.push(e);
+    }
+  }
+
+  console.log('p: ', p);
+  console.log('a: ', a);
+  console.log('z: ', z);
+
+  return (a.length > 1 ? quickSort(a) : a).concat(p).concat(z.length > 1 ? quickSort(z) : z);
+}
+
