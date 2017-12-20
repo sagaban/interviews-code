66df405e0c0e19d0e9d7a2506cedbf62ee865cb6
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2017-12-20 18:19:06 -0300

    Config VSCode debug task

--- test/sorting/quick-sort-test.js
+++ test/sorting/quick-sort-test.js
@@ -7,7 +7,6 @@ describe('QuickSort sorting algorithm', () => {
     const toSort = [5, 2, 3, 10, 12, 7, 7, 7, 14, 9, -3, 6, 11, 0];
     const expected = [-3, 0, 2, 3, 5, 6, 7, 7, 7, 9, 10, 11, 12, 14];
 
-    debugger;
     const sorted = quickSort(toSort);
     assert.deepEqual(expected, sorted, 'The array is sorted properly');
   });
