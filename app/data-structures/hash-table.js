d18d52f95d3abd12d5495e6a4b4f97bff3c4795c
Santiago Bandiera <santiagobandiera@yahoo.com.ar>
2017-12-28 19:41:22 -0300

    Add methos to HashTable. Still failing tests

--- app/data-structures/hash-table.js
+++ app/data-structures/hash-table.js
@@ -1,3 +1,28 @@
 export default function HashTable() {
-  const table = []; // eslint-disable-line no-unused-vars
+  const table = [];
+
+  const loseloseHashCode = (key) => {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += key.charCodeAt(i);
+    }
+    return hash % 37;
+  };
+
+  // This adds a new item to the hash table (or it can also update it)
+  this.put = function(key, value) {
+    const position = loseloseHashCode(key);
+    // console.log(position + ' - ' + key);
+    table[position] = value;
+  };
+
+  // This returns a specific value searched by the key
+  this.get = function(key) {
+    return table[loseloseHashCode(key)];
+  };
+
+  // This removes the value from the hash table using the key
+  this.remove = function(key) {
+    table[loseloseHashCode(key)]= undefined;
+  };
 }
--- test/data-structures/hash-table-test.js
+++ test/data-structures/hash-table-test.js
@@ -3,11 +3,47 @@ import HashTable from '/app/data-structures/hash-table';
 
 describe('HashTable data structure', () => {
   let hashTable;
+  const elements = [ [ 'a', 2 ], [ 'b', 7 ], [ 'c', 13 ] ];
   beforeEach(function() {
     hashTable = new HashTable();
+    elements.forEach((element) => hashTable.put(element[0], element[1]));
   });
 
-  it('exists', () => {
+  it('Put and get values', () => {
     assert.ok(!!hashTable, 'hashTable exists');
+
+    elements.forEach((element, index) =>
+      assert.equal(
+        hashTable.get(element[0]),
+        element[1],
+        `element value at #${index} match`
+      )
+    );
+  });
+
+  it('Does not overwrite elements with the same hash', () => {
+    hashTable = new HashTable();
+    const sameHashElements = [ [ 'ab', 2 ], [ 'y', 7 ] ];
+    sameHashElements.forEach((element) =>
+      hashTable.put(element[0], element[1])
+    );
+
+    sameHashElements.forEach((element, index) =>
+      assert.equal(
+        hashTable.get(element[0]),
+        element[1],
+        `element value at #${index} match`
+      )
+    );
+  });
+
+  it('Remove a certain value', () => {
+    const keyToRemove = 'b';
+    hashTable.remove(keyToRemove);
+    assert.equal(hashTable.get(keyToRemove), undefined, 'element removed');
+  });
+
+  it('Get a undefined for an element that not exist', () => {
+    assert.equal(hashTable.get('pepe'), undefined, 'element does not exists');
   });
 });
